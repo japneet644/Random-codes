51e8cce911c1afe4012ff53bc295c6eb78a2416c
Japneet Singh <37984270+japneet644@users.noreply.github.com>
2024-02-25 12:09:12 -0500

    Add files via upload

---
+++ Aritrasimulation2.m
@@ -0,0 +1,112 @@
+clc; clear all; close all;
+l1w1t1 = [0.505,0.505,0.505,0.501];
+l1w1t2 = [0.50,0.5,0.5,0.5];
+T = 50000;
+nature = rand>0.5
+prior = [0.5,0.5,0.5,0.5];
+sharedprior = [0.5,0.5,0.5,0.5];
+sharedprior2 = [0.5,0.5,0.5,0.5];
+sharedprior3 = [0.5,0.5,0.5,0.5];
+
+U = zeros(T,4);
+V = zeros(T,4);
+W = zeros(T,4);
+X = zeros(T,4);
+
+for t = 1:T
+    if nature==0
+        signal = rand([1,4]) > l1w1t1;
+    else
+        signal = rand([1,4]) > l1w1t2;
+    end
+    for n = 1:4
+        if signal(n) == 0
+            
+            prior(n) = prior(n)*l1w1t1(n)/(prior(n)*l1w1t1(n) + (1-prior(n))*l1w1t2(n));
+            
+        else 
+            prior(n) = prior(n)*(1-l1w1t1(n))/(prior(n)*(1-l1w1t1(n)) + (1-prior(n))*(1-l1w1t2(n)));
+        end
+        U(t,:) = prior;
+    end
+
+    gg = sharedprior(1);
+    dummyvar = [prior(1),sharedprior(2:4)];
+    sharedprior(1) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+    dummyvar = [prior(2),gg];
+    sharedprior(2) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+
+    dummyvar = [prior(3),gg];
+    sharedprior(3) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+    dummyvar = [prior(4),gg];
+    sharedprior(4) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+    V(t,:) = sharedprior;
+%%%% first local bayesian update then sharing of beliefs and aggregation 
+    
+    for n = 1:4
+        if signal(n) == 0
+            sharedprior2(n) = sharedprior2(n)*l1w1t1(n)/(sharedprior2(n)*l1w1t1(n) + (1-sharedprior2(n))*l1w1t2(n));
+        else 
+            sharedprior2(n) = sharedprior2(n)*(1-l1w1t1(n))/(sharedprior2(n)*(1-l1w1t1(n)) + (1-sharedprior2(n))*(1-l1w1t2(n)));
+        end
+    end
+    oldsharedprior = sharedprior2;
+    dummyvar = [oldsharedprior(1:4)];
+    sharedprior2(1) = min(dummyvar )/(min(dummyvar ) + min(1-dummyvar ));
+    dummyvar = [oldsharedprior(2),oldsharedprior(1)];
+    sharedprior2(2) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+
+    dummyvar = [oldsharedprior(3),oldsharedprior(1)];
+    sharedprior2(3) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+    dummyvar = [oldsharedprior(4),oldsharedprior(1)];
+    sharedprior2(4) = min(dummyvar)/(min(dummyvar) + min(1-dummyvar));
+    W(t,:) = sharedprior2;
+%%%% first aggregation and THEN LOCAL BAYESIAN UPDATE
+    oldsharedprior = sharedprior3;
+    dummyvar = oldsharedprior(1:4);
+    if signal(1) == 0
+        sharedprior3(1) = min(dummyvar)*l1w1t1(1)/(min(dummyvar)*l1w1t1(1) + min(1-dummyvar)*l1w1t2(1));
+    else 
+        sharedprior3(1) = min(dummyvar)*(1-l1w1t1(1))/(min(dummyvar)*(1-l1w1t1(1)) + min(1-dummyvar)*(1-l1w1t2(1)));
+    end
+
+    dummyvar = [oldsharedprior(1),oldsharedprior(2)];
+    if signal(2) == 0
+        sharedprior3(2) = min(dummyvar)*l1w1t1(2)/(min(dummyvar )*l1w1t1(2) + min(1-dummyvar )*l1w1t2(2));
+    else 
+        sharedprior3(2) = min(dummyvar)*(1-l1w1t1(2))/(min(dummyvar )*(1-l1w1t1(2)) + min(1-dummyvar )*(1-l1w1t2(2)));
+    end
+
+    dummyvar = [oldsharedprior(1),oldsharedprior(3)];
+    if signal(3) == 0
+        sharedprior3(3) = min(dummyvar)*l1w1t1(2)/(min(dummyvar )*l1w1t1(2) + min(1-dummyvar )*l1w1t2(2));
+    else 
+        sharedprior3(3) = min(dummyvar)*(1-l1w1t1(2))/(min(dummyvar )*(1-l1w1t1(2)) + min(1-dummyvar )*(1-l1w1t2(2)));
+    end
+    dummyvar = [oldsharedprior(1),oldsharedprior(4)];
+    if signal(4) == 0
+        sharedprior3(4) = min(dummyvar)*l1w1t1(2)/(min(dummyvar )*l1w1t1(2) + min(1-dummyvar )*l1w1t2(2));
+    else 
+        sharedprior3(4) = min(dummyvar)*(1-l1w1t1(2))/(min(dummyvar )*(1-l1w1t1(2)) + min(1-dummyvar )*(1-l1w1t2(2)));
+    end
+    X(t,:) = sharedprior3; 
+end
+figure(1)
+plot(U(:,1))
+hold on
+plot(U(:,2))
+figure(2)
+plot(V(:,1))
+ hold on
+plot(V(:,2))
+ figure(3)
+%hold on
+plot(W(:,1))
+ hold on
+% plot(W(:,2))
+%figure(4)
+plot(X(:,1))
+%hold on
+%plot(X(:,2))
+hold on
+plot(U(:,1))
